a7e498109ae68b0e5699b4990457e7d73b0bbd66
Michael McFarland <mikedmcfarland@gmail.com>
2015-01-23 14:48:20 -0500

    starting implemenation
    works but doesn't have any means to control
    the context, or the ports involved

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp')
+
+require('engulped')(gulp)
+gulp.task('default',['build','test'])
+gulp.task('run', ['build','test'], function() {
+  require('./dist/')
+})
+
+gulp.task('run-client', ['build','test'], function() {
+  new require('./dist/connectClient')
+})
+
+gulp.task('run-one-client', ['build','test'], function() {
+  new require('./dist/connectOne')
+})
+
+gulp.task('run-one-server', ['build','test'], function() {
+  new require('./dist/serveOne')
+})
+
+gulp.task('run-server', ['build','test'], function() {
+  new require('./dist/manageConnections')
+})
